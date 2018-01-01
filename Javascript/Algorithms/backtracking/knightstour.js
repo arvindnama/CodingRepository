874a2278413a7b13d37d8fe81e5e6f5b78ec3d89
Nama <Arvind.Umashankar@philips.com>
2018-01-01 01:04:51 -0800

    backtracking

--- Javascript/Algorithms/backtracking/knightstour.js
+++ Javascript/Algorithms/backtracking/knightstour.js
@@ -1,6 +1,7 @@
 /*
 * knight Tour Problem::
-* The knight is placed on the first block of an empty board and, moving according to the rules of chess, must visit each square exactly once
+* The knight is placed on the first block of an empty board and, 
+* moving according to the rules of chess, must visit each square exactly once
 */
 
 let knightTour = function(){
