072dd662790bc9b5f922d8f240a53a64e8c29ff2
Nama <Arvind.Umashankar@philips.com>
2018-01-26 10:30:53 -0800

    ES6: clean up

--- Javascript/DataStructure/arrays/sortwave.js
+++ Javascript/DataStructure/arrays/sortwave.js
@@ -3,17 +3,20 @@ var printArray = function(a){
 };
 
 var swap = function(a, i , j){
-  var t = a[i]; a[i] =a[j]; a[j] = t;
+  [a[i],a[j]] = [a[j],a[i]];
 };
 
 var sortwave = function(a){
   var n = a.length;
+  var swap = ( i , j)=>{
+    [a[i],a[j]] = [a[j],a[i]];
+  };
   for(var i = 0 ; i < n -1 ; i+=2){
     if(i > 0 &&  a[i-1] > a[i]){
-      swap(a,i , i-1);
+      swap(i , i-1);
     }
     if(i < n -1 && a[i] < a[i+1]){
-      swap(a,i , i+1);
+      swap(i , i+1);
     }
   }
   return a;
